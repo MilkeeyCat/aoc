70e697beba96f7bb31ff0d7e9681aed3e4dbbcf6
MilkeeyCat <milkeeycat@gmail.com>
2025-12-01 12:53:42 +0200

    day1(2025): solution in C++

---
+++ 2025/day1/part2.cpp
@@ -0,0 +1,48 @@
+#include <cstdint>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+constexpr uint8_t MaxValue = 99;
+
+int main() {
+	std::ifstream File("./input");
+	std::stringstream FileContent;
+	std::string Line;
+	uint16_t Value = 50;
+	size_t Result = 0;
+
+	FileContent << File.rdbuf();
+
+	while(std::getline(FileContent, Line, '\n')) {
+		bool Clockwise = Line[0] == 'R';
+		uint16_t Amount = std::stoi(Line.substr(1));
+		int16_t TmpValue = 0;
+
+		if(Clockwise) {
+			TmpValue = (int16_t)Value + (int16_t)Amount;
+		} else {
+			TmpValue = (int16_t)Value - (int16_t)Amount;
+		}
+
+		Result += Amount / (MaxValue + 1);
+
+		uint16_t Remainder = Amount % (MaxValue + 1);
+
+		if(
+			Value != 0 &&
+			((Clockwise && (Value + Remainder) > MaxValue) || (!Clockwise && (int16_t)(Value - Remainder) <= 0))) {
+			++Result;
+		}
+
+		if(TmpValue >= 0) {
+			Value = TmpValue % (MaxValue + 1);
+		} else {
+			Value = ((TmpValue % (MaxValue + 1)) + (MaxValue + 1)) % (MaxValue + 1);
+		}
+	}
+
+	std::cout << "result: " << Result << "\n";
+}
